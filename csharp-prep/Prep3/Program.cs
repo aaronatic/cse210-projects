3046f9c5e41a234ffada82c82626148929e047c0
AguilarAaron <aaguilar@habiltec.com>
2023-01-22 09:18:08 -0600

    Prep3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 101);
+        int gNumber = -1;
+
+        while (gNumber != mNumber)
+        {
+
+        Console.WriteLine("What is your guess?");
+        gNumber = int.Parse(Console.ReadLine());
+
+        if (gNumber < mNumber)
+        {Console.WriteLine("Higher"); }
+        else if (gNumber > mNumber)
+        {Console.WriteLine("Lower"); }
+        else
+        {Console.WriteLine("That is exactly right"); }
+        
+        }
     }
 }
