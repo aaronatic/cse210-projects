e48f8fe541dad9f76aa220440c3a41f401a2e38b
AguilarAaron <aaguilar@habiltec.com>
2023-01-21 21:17:03 -0600

    Changes I made to work

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,43 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your grade percentage? ");
-        string gradepercentage = Console.ReadLine();
-        int percent = int.Parse(gradepercentage);
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
-        string letter = "";
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
 
-        if (percent >= 90)
-        {
-            letter = "A";
-        }
-         else if (percent >= 80)
-        {
-            letter = "B";
-        }
-         else if (percent >= 70)
-        {
-            letter = "C";
-        }
-        else if (percent >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
-
-        Console.WriteLine($"Your grade is: {letter}");
-
-        if (percent >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("Better luck next time!");
-        }
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
 
     }
 }
